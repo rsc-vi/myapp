53ff11f154741de3a1ef156bcd7663245254e51a
Girleide de Araujo <145593247+NathalyaKSF@users.noreply.github.com>
2024-07-10 10:48:33 -0300

    lembrete

---
+++ clienteRequest.js
@@ -0,0 +1,8 @@
+
+//////////////////estudar ////////////////////////
+//////////atv no sigaa///////////////////////////
+
+
+// -->  https://www.freecodecamp.org/portuguese/news/tutorial-de-fetch-api-em-javascript-exemplos-de-post-e-cabecalho/
+//  --> https://expressjs.com/pt-br/guide/routing.html
+// --> https://expressjs.com/pt-br/starter/installing.html
