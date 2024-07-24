e63fda0098458d11f914586da3b42668730ef4b6
Girleide de Araujo <145593247+NathalyaKSF@users.noreply.github.com>
2024-07-24 10:40:22 -0300

    d

---
+++ exercicio-trycath.js
@@ -0,0 +1,24 @@
+try {
+    carregando = true;
+
+    let a = 20;
+    let b = null;
+    
+    if(!b) throw new Error("ERRO DE DIVISÃO") 
+    let result = a / b;
+    if(result < 1) throw new Error("A DIVISÃO NÃO PODE SER MENOR Q 1")
+    console.log("calculou!");
+    console.log(result);
+    
+} catch (error) {
+   
+    console.log("ERRO!");
+    console.log(error.message)
+    
+   
+}finally{
+
+    carregando = false;
+    console.log("ABAGARAS")
+
+}
